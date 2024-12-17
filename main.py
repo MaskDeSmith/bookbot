e6d455061c9f8377d2869a62fb96489a8d681807
MaskDeSmith <jasondylanelmer@gmail.com>
2024-12-17 09:37:32 -0800

    modified main.py to clean functions and add word count

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-if __name__ == "__main__":
-    main()
+    book_path = "books/frankenstein.txt"
+    full_text = get_book_text(book_path)
+    word_count = get_word_count(full_text)
+    print(f"{word_count} is the word count")
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+def get_word_count(full_text):
+    words = full_text.split()
+    return len(words)
+main()
